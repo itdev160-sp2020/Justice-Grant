35b196559cf333665b9b73276d11bdfafce70b33
Guynamedjustice <you@example.com>
2020-03-10 16:55:57 -0500

    Completed Activity 4

---
+++ activity-4/js/scripts.js
@@ -0,0 +1,18 @@
+// welcome message
+var user = 'Jahlen';
+var salutation = 'Hello,  ';
+var greeting = salutation + user + '! Check out this awesome innovative vehicle';
+var greetingEl = document.getElementById('greeting');
+
+greetingEl.textContent = greeting;
+
+// product price
+var price = 20,
+    studentDiscount = .10,
+    studentPrice = price - (price * studentDiscount),
+    priceEl = document.getElementById('price'),
+    studentPriceEl = document.getElementById('student-price');
+
+priceEl.textContent = price.toFixed(2);
+studentPrice.textContent = studentPrice.toFixed(2);
+
